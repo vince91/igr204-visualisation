ba08ee0f3a5a6c9e614d0b3d5df462b84901c1ba
Vincent Timofti <vincent.timofti@gmail.com>
2016-06-06 19:32:07 +0200

    country opacity in function of their hapiness

--- js/design2.js
+++ js/design2.js
@@ -1,46 +1,61 @@
 var svg = null;
 var dataset = null;
 var count = 0;
+var color_opacity = null;
+
 function draw() {
-	console.log("draw");
-	console.log(svg);
-	svg.selectAll(".europe")
-	   .datum(function(d) { return { countryCode: d3.select(this).attr("id") }})
-	   .data(dataset, function(d) { return d.countryCode })
-	   .style("fill", "red");
-	   //.each(function(d) { console.log(this.id, d, ++count)});
-}
+    svg.selectAll(".europe")
+       .datum(function(d) { return { countryCode: d3.select(this).attr("id") }})
+       .data(dataset, function(d) { return d.countryCode })
+       .style("fill", "red")
+       .style("fill-opacity", function(d) {
+            return color_opacity(d.mean);
+       });
+};
+
+function mouse_over() {
+    svg.selectAll(".europe")
+       .on("mouseover", function(d) {
+            $("#country").text(d.countryCode);
+            $("#mean").text(d.mean);
+       })
+       .on("mouseout", function() {
+            $("#country").text('');
+            $("#mean").text('');
+       })
+};
 
 $(document).ready(function() {
 
-	// add svg to page
+    // add svg to page
     d3.xml("svg/europe.svg", "image/svg+xml", function(error, xml) {
         if (error) throw error;
-        $("body").append(xml.documentElement);
+        $("svg").replaceWith(xml.documentElement);
         svg = d3.select("svg");
 
         // load csv
         var dsv = d3.dsv(";", "text/plain")
         dsv("data/all_questions.csv", function(d) {
-        	return {
-        		countryCode: d.CountryCode.toLowerCase(),
-        		questionCode: d.question_code,
-        		subset: d.subset,
-        		answer: d.answer,
-        		mean: +d.Mean
-        	}
+            return {
+                countryCode: d.CountryCode.toLowerCase(),
+                questionCode: d.question_code,
+                subset: d.subset,
+                answer: d.answer,
+                mean: +d.Mean
+            }
         }, function(error, rows) {
-        	console.log(rows[0]);
-        	dataset = rows.filter(function(row) {
-        		return row.questionCode == "Y11_Q41"
-        	})
-        	console.log(dataset)
-        	draw();
+            console.log(rows[0]);
+            dataset = rows.filter(function(row) {
+                return row.questionCode == "Y11_Q41"
+            });
+
+            color_opacity = d3.scale.linear()
+                            .domain(d3.extent(dataset, function(row) {
+                                return row.mean;
+                            })).range([0.3, 1]);
+            draw();
+            mouse_over();
 
         });
     });
-
-
-
-
 });
